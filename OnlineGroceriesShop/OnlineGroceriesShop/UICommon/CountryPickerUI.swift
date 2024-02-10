76a3209d4db4114462d9b843ed3b66604748cb6b
Amar Asagekar <amarasagekar@gmail.com>
2024-02-10 22:55:23 +0530

    Implemented Sign  in view with mobile  and country flag with country code

---
+++ OnlineGroceriesShop/OnlineGroceriesShop/UICommon/CountryPickerUI.swift
@@ -0,0 +1,18 @@
+//
+//  CountryPickerUI.swift
+//  OnlineGroceriesShop
+//
+//  Created by AMAR on 10/02/24.
+//
+
+import SwiftUI
+
+struct CountryPickerUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountryPickerUI()
+}
