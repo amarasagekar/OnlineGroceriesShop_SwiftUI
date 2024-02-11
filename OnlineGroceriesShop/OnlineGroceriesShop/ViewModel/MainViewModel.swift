384f9790b838f06c14ab7eea2be5e051d91f3a7d
Amar Asagekar <amarasagekar@gmail.com>
2024-02-11 15:36:12 +0530

    UI for Login View

---
+++ OnlineGroceriesShop/OnlineGroceriesShop/ViewModel/MainViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewModel.swift
+//  OnlineGroceriesShop
+//
+//  Created by AMAR on 11/02/24.
+//
+
+import SwiftUI
+
+struct MainViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainViewModel()
+}
